dd57422f3751dc7354c2aaa7009fd8d7af89305b
cs_apple_08 <cs_apple_08@csapple08simac.aamu.edu>
2019-10-24 20:32:17 -0500

    Added the grid view tab to the application

---
+++ Flix_App/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix_App
+//
+//  Created by cs_apple_08 on 10/24/19.
+//  Copyright © 2019 E INC. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
